01d4a9e8a48a6977a340472462c269d2d84c5e63
Ragnar Hedenskog <ragnar.hedenskog@gmail.com>
2018-11-01 23:16:14 +0100

    Updates

---
+++ precache-manifest.81a9dc22708642b6d8eff54f0661125a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8db157963e4d9eef233",
+    "url": "/flower/static/js/runtime~main.d8db1579.js"
+  },
+  {
+    "revision": "6a1f193fba00c3cbfb59",
+    "url": "/flower/static/js/main.6a1f193f.chunk.js"
+  },
+  {
+    "revision": "720216d02cf0093bca4f",
+    "url": "/flower/static/js/1.720216d0.chunk.js"
+  },
+  {
+    "revision": "6a1f193fba00c3cbfb59",
+    "url": "/flower/static/css/main.dd435022.chunk.css"
+  },
+  {
+    "revision": "456c9cf3c6f9540439bc90abc42f955f",
+    "url": "/flower/index.html"
+  }
+];
